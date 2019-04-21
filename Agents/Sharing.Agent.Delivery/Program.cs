fdd5add9ead70486b8557da37e7cd9055d1e3be8
Manny Qiao <v-jinhqi@microsoft.com>
2019-04-21 17:02:27 +0800

    [Joey]

---
+++ Agents/Sharing.Agent.Delivery/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sharing.Agent.Delivery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
